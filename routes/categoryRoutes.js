b84b4370cf9e4c143061b13dc8b0131ac2dc57f8
SAURABH KUMAR <saurabhsinghania111@gmail.com>
2025-02-03 18:54:25 +0530

    Add files via upload
    COMPLETED

---
+++ routes/categoryRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js';
+import { categoryController, createCategoryController, deleteCategoryController, singleCategoryController, updateCategoryController } from '../controllers/categoryController.js';
+const router=express.Router()
+//Routes
+//create category route
+router.post('/create-category',requireSignIn,isAdmin,createCategoryController)
+//update category route
+router.put('/update-category/:id',requireSignIn,isAdmin,updateCategoryController)
+//get all category
+router.get('/get-category',categoryController)
+//get single category route
+router.get('/single-category/:slug',singleCategoryController)
+//delete category 
+router.delete('/delete-category/:id',requireSignIn,isAdmin,deleteCategoryController)
+export default router;
